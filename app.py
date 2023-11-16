71757f82837452d68565822da748cf00f2d519af
ashutosh-shukla <121450702+ashutosh-shukla@users.noreply.github.com>
2023-11-16 12:15:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ from pandas_datareader import data as pdr
 import yfinance as yfin
 import streamlit as st
 from keras.models import load_model
-from tensorflow.keras.models import load_model
-from custom_optimizer_module import CustomAdamOptimizer  # Import your custom optimizer
-
-
+from sklearn.preprocessing import MinMaxScaler
 
 start = '2010-01-01'
 end = '2023-10-20'
@@ -19,6 +16,7 @@ user_input = st.text_input('Enter Stock Ticker', 'AAPL')
 yfin.pdr_override()
 df = pdr.get_data_yahoo(user_input, start, end)
 
+# ... (Visualization code as before)
 # Describing data
 st.subheader('Data from 2010 to 2023')
 st.write(df.describe())
@@ -50,32 +48,12 @@ st.pyplot(fig)
 data_train = pd.DataFrame(df['Close'][0:int(len(df)*0.80)])
 data_test = pd.DataFrame(df['Close'][int(len(df)*0.80):int(len(df))])
 
-from sklearn.preprocessing import MinMaxScaler
-scaler=MinMaxScaler(feature_range=(0,1))
-
-data_train_array=scaler.fit_transform(data_train)
-
-
-# #splitting data into xtrain and ytrain
-# x_training=[]
-# y_training=[]
-# for i in range(100,data_train_array.shape[0]):
-#     x_training.append(data_train_array[i-100:i])
-#     y_training.append(data_train_array[i,0])
-    
-# x_training,y_training=np.array(x_training),np.array(y_training)
-
-
-#load my model
-
+scaler = MinMaxScaler(feature_range=(0, 1))
+data_train_array = scaler.fit_transform(data_train)
 
 # Load your Keras model from the file
+model = load_model('keras_model.h5')
 
-custom_objects = {'CustomAdamOptimizer': CustomAdamOptimizer}
-model = load_model('keras_model.h5', custom_objects=custom_objects)
-
-
-# The rest of your code
 past_100 = data_train.tail(100)
 final_df = pd.concat([past_100, data_test], ignore_index=True)
 input_data = scaler.fit_transform(final_df)
@@ -89,9 +67,9 @@ for i in range(100, input_data.shape[0]):
 x_test_array, y_test_array = np.array(x_test), np.array(y_test)
 y_predicted = model.predict(x_test_array)
 
-scaler = scaler.scale_
+scaler_scale = scaler.scale_
 # Now, inverse the scaling
-scale_factor = 1 / scaler[0]
+scale_factor = 1 / scaler_scale[0]
 
 y_predicted = y_predicted * scale_factor
 y_test_array = y_test_array * scale_factor
